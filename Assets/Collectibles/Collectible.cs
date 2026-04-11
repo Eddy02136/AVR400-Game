96f389fef26c8937be5a8e0e8b036dcc12e10453
Eddy <eddygardes@gmail.com>
2026-04-10 21:21:58 -0700

    feat: add all collectible items with floating text and personalized pickup descriptions

--- Assets/Collectibles/Collectible.cs
+++ Assets/Collectibles/Collectible.cs
@@ -1,42 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
 
     public GameObject itemPrefab;
+    public string objectName;
+    private TextMeshProUGUI floatText;
+
+    public string dialogue;
+
+    private bool playerInRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        floatText = GetComponentInChildren<TextMeshProUGUI>();
+        floatText.text = "???";
+        playerInRange = false;
+
+        if (itemPrefab != null)
+        {
+            // Add item child
+            GameObject item = Instantiate(itemPrefab, transform.position, transform.rotation);
+            item.transform.SetParent(transform);
+            item.transform.localPosition = Vector3.zero;
+
+            // Position floatText above item
+            Renderer rend = item.GetComponentInChildren<Renderer>();
+            if (rend != null)
+            {
+                floatText.rectTransform.position = new Vector3(transform.position.x, rend.bounds.max.y + 0.5f, transform.position.z);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            GameManager.GM.hintsCollected++;
+            GetComponent<Collider>().enabled = false;
+            GameManager.GM.collectibleHelpText.enabled = false;
+            GameManager.GM.StartDialogue(dialogue);
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject, 0.5f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         GameManager.GM.collectibleHelpText.enabled = true;
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            GameManager.GM.hintsCollected++;
-            GetComponent<Collider>().enabled = false;
-            GameManager.GM.collectibleHelpText.enabled = false;
-            Destroy(gameObject, 0.5f);
-        }
+        floatText.text = objectName;
+        playerInRange = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         GameManager.GM.collectibleHelpText.enabled = false;
+        playerInRange = false;
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,9 +13,19 @@ public class GameManager : MonoBehaviour
 
     float timer;
 
-    public TextMeshProUGUI hintCollectedText, timerText, collectibleHelpText;
+    public TextMeshProUGUI hintCollectedText, timerText, collectibleHelpText, dialogueText;
 
     public static GameManager GM;
+
+    public GameObject door;
+
+    public GameObject dialogueBg;
+
+    private bool doorIsOpen;
+
+    public float dialogueSpeed;
+
+    private string dialogue;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +39,10 @@ public class GameManager : MonoBehaviour
             GM = this;
         }
         collectibleHelpText.enabled = false;
+        doorIsOpen = false;
+        dialogueText.text = "";
+        dialogueText.enabled = false;
+        dialogueBg.SetActive(false);
     }
 
     // Update is called once per frame
@@ -38,5 +53,40 @@ public class GameManager : MonoBehaviour
 
         hintCollectedText.text = "Hints Collected: " + hintsCollected + "/" + totalHints;
         timerText.text = string.Format("{0:00}:{1:00}:{2:00}", t.Minutes, t.Seconds, t.Milliseconds);
+
+        if (hintsCollected >= totalHints && !doorIsOpen)
+        {
+            door.transform.Rotate(Vector3.up * (door.transform.rotation.y - 111f));
+            doorIsOpen = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dialogueText.text == dialogue)
+            {
+                StopAllCoroutines();
+                dialogueText.text = "";
+                dialogueBg.SetActive(false);
+                dialogueText.enabled = false;
+            }
+        }
+    }
+
+    public void StartDialogue(string dialogue)
+    {
+        this.dialogue = dialogue;
+        dialogueBg.SetActive(true);
+        dialogueText.enabled = true;
+        dialogueText.text = "";
+        StartCoroutine(TypeLine(dialogue));
+    }
+
+    IEnumerator TypeLine(string dialogue)
+    {
+        foreach (char c in dialogue)
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(dialogueSpeed);
+        }
     }
 }
